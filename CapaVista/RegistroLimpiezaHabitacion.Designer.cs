cafd14ac8bd46763452e5120da2d639c2cd2c8bc
MAYL H.C <herbascaseresguido@gmail.com>
2023-06-12 20:47:37 -0300

    Se modifico el Form de Limpieza

--- CapaVista/RegistroLimpiezaHabitacion.Designer.cs
+++ CapaVista/RegistroLimpiezaHabitacion.Designer.cs
@@ -29,17 +29,23 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistroLimpiezaHabitacion));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             label1 = new Label();
             groupBox1 = new GroupBox();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
             dateTimePicker1 = new DateTimePicker();
             guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            comboBox2 = new ComboBox();
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
@@ -48,12 +54,12 @@
             label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Microsoft Sans Serif", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Alegreya", 17.9999981F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = Color.FromArgb(190, 158, 68);
             label1.ImageAlign = ContentAlignment.TopLeft;
-            label1.Location = new Point(333, 31);
+            label1.Location = new Point(291, 23);
             label1.Name = "label1";
-            label1.Size = new Size(384, 36);
+            label1.Size = new Size(274, 35);
             label1.TabIndex = 0;
             label1.Text = "REGISTRO DE LIMPIEZA";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -62,100 +68,148 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(textBox1);
-            groupBox1.Location = new Point(309, 81);
-            groupBox1.Margin = new Padding(3, 4, 3, 4);
+            groupBox1.Location = new Point(270, 61);
             groupBox1.Name = "groupBox1";
-            groupBox1.Padding = new Padding(3, 4, 3, 4);
-            groupBox1.Size = new Size(633, 311);
+            groupBox1.Size = new Size(554, 233);
             groupBox1.TabIndex = 4;
             groupBox1.TabStop = false;
             groupBox1.Text = "groupBox1";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(-170, 51);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(-149, 38);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(163, 27);
+            textBox1.Size = new Size(143, 23);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(14, 119);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(12, 89);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(149, 27);
+            textBox2.Size = new Size(131, 23);
             textBox2.TabIndex = 5;
-            // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(14, 191);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(188, 27);
-            textBox3.TabIndex = 6;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(14, 255);
-            textBox4.Margin = new Padding(3, 4, 3, 4);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(197, 27);
-            textBox4.TabIndex = 7;
-            textBox4.TextChanged += textBox4_TextChanged;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(14, 320);
-            textBox5.Margin = new Padding(3, 4, 3, 4);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(220, 27);
-            textBox5.TabIndex = 8;
             // 
             // dateTimePicker1
             // 
-            dateTimePicker1.Location = new Point(14, 376);
-            dateTimePicker1.Margin = new Padding(3, 4, 3, 4);
+            dateTimePicker1.Location = new Point(12, 210);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(261, 27);
+            dateTimePicker1.Size = new Size(229, 23);
             dateTimePicker1.TabIndex = 1;
             // 
             // guna2Button2
             // 
             guna2Button2.BackgroundImage = (Image)resources.GetObject("guna2Button2.BackgroundImage");
             guna2Button2.BackgroundImageLayout = ImageLayout.None;
-            guna2Button2.CustomizableEdges = customizableEdges1;
+            guna2Button2.CustomizableEdges = customizableEdges7;
             guna2Button2.DisabledState.BorderColor = Color.DarkGray;
             guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
             guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
             guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
             guna2Button2.FillColor = Color.Transparent;
-            guna2Button2.Font = new Font("Microsoft Sans Serif", 11.2499981F, FontStyle.Bold, GraphicsUnit.Point);
+            guna2Button2.Font = new Font("Alegreya", 11.2499981F, FontStyle.Bold, GraphicsUnit.Point);
             guna2Button2.ForeColor = Color.FromArgb(190, 158, 68);
-            guna2Button2.Location = new Point(54, 440);
-            guna2Button2.Margin = new Padding(3, 4, 3, 4);
+            guna2Button2.Location = new Point(47, 330);
             guna2Button2.Name = "guna2Button2";
-            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2Button2.Size = new Size(181, 41);
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            guna2Button2.Size = new Size(158, 31);
             guna2Button2.TabIndex = 9;
             guna2Button2.Text = "REGISTRAR";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Alegreya", 9.749998F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(190, 158, 68);
+            label2.Location = new Point(22, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 19);
+            label2.TabIndex = 1;
+            label2.Text = "ID Registro";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Alegreya", 9.749998F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(190, 158, 68);
+            label3.Location = new Point(11, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 19);
+            label3.TabIndex = 10;
+            label3.Text = "Numero de Habitacion";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(11, 137);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(132, 23);
+            comboBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Alegreya", 9.749998F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(190, 158, 68);
+            label4.Location = new Point(11, 177);
+            label4.Name = "label4";
+            label4.Size = new Size(40, 19);
+            label4.TabIndex = 12;
+            label4.Text = "Fecha";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Alegreya", 9.749998F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(190, 158, 68);
+            label5.Location = new Point(12, 249);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 19);
+            label5.TabIndex = 13;
+            label5.Text = "Estado";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 271);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(229, 23);
+            comboBox2.TabIndex = 14;
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackgroundImage = (Image)resources.GetObject("guna2Panel1.BackgroundImage");
+            guna2Panel1.BackgroundImageLayout = ImageLayout.Stretch;
+            guna2Panel1.CustomizableEdges = customizableEdges9;
+            guna2Panel1.Location = new Point(22, 12);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            guna2Panel1.Size = new Size(54, 46);
+            guna2Panel1.TabIndex = 15;
+            // 
             // RegistroLimpiezaHabitacion
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaptionText;
-            ClientSize = new Size(974, 497);
+            ClientSize = new Size(852, 373);
+            Controls.Add(guna2Panel1);
+            Controls.Add(comboBox2);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(guna2Button2);
             Controls.Add(dateTimePicker1);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(groupBox1);
             Controls.Add(label1);
             ForeColor = SystemColors.ControlLightLight;
-            Margin = new Padding(3, 4, 3, 4);
             Name = "RegistroLimpiezaHabitacion";
             Text = "RegistroLimpiezaHabitacion";
             Load += RegistroLimpiezaHabitacion_Load;
@@ -171,10 +225,14 @@
         private GroupBox groupBox1;
         private TextBox textBox1;
         private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
         private DateTimePicker dateTimePicker1;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Label label2;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
--- CapaVista/RegistroLimpiezaHabitacion.cs
+++ CapaVista/RegistroLimpiezaHabitacion.cs
@@ -31,5 +31,10 @@ namespace CapaVista
         {
 
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
